f7544b6af153694370c7cb3e511d5750bb695a1e
yan mello <yan_mvd@hotmail.com>
2023-06-02 12:22:18 -0300

    test: ensure dpk will return the same key if its size is lower than 256

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,10 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("should return the given key it it is lower than 256", () => {
+    const fakeKey = "mockedPartitionKey"
+    const trivialKey = deterministicPartitionKey({ partitionKey: fakeKey });
+    expect(trivialKey).toBe(fakeKey);
+  })
 });
